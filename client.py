341bc37baaa2eec1b606f1e01d812bcecfb31368
raghad3693 <105462865+raghad3693@users.noreply.github.com>
2022-06-16 19:36:17 +0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,36 @@
+from concurrent.futures import thread
+import socket
+import threading
+
+# Choosing Nickname
+nickname = input("Choose your name: ")
+
+# Connecting To Server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 55555))
+
+# Listing to Server and Sending Nickname
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            print("An error occured!")
+            client.close()
+            break
+
+# Sending Messages To Server
+def write():
+    while True:
+        message = '{}: {}'.format(nickname, input(''))
+        client.send(message.encode('ascii'))
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
